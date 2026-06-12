4a985be34f1377d2fb64362e2c2f560302278443
comit <alireza2040afshari@gmail.com>
2026-06-12 19:03:50 +0330

    complete base

--- test.py
+++ test.py
@@ -14,25 +14,36 @@ def MP3_saver():
 
     return file_path
 
-def cut(audio = ""):
-    audio = AudioSegment.from_file(audio)
+
+def cut_audio(file_path):
+    audio = AudioSegment.from_file(file_path)
+
     start = int(input("Start second: "))
     end = int(input("End second: "))
+
     clip = audio[start * 1000:end * 1000]
-    clip.export("output.mp3", format="mp3")
-    clip = cut(clip)
 
+    return clip
+
+
+def save_audio(clip):
     save_path = filedialog.asksaveasfilename(
-    title="Save MP3",
-    defaultextension=".mp3",
-    filetypes=[("MP3 Files", "*.mp3")])
+        title = "Save MP3",
+        defaultextension = ".mp3",
+        filetypes = [("MP3 Files", "*.mp3")]
+    )
 
-    clip.export(save_path, format="mp3")
+    if save_path:
+        clip.export(save_path, format="mp3")
 
 
 def main() -> None:
     file_path = MP3_saver()
-    file_path = cut(file_path)
+
+    if file_path:
+        clip = cut_audio(file_path)
+        save_audio(clip)
+    
  
 
 if __name__ == "__main__":
